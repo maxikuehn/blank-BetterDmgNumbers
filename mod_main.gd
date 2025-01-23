ecc220fd38f3ae9e1a0c1cb5f14b389536ef7dfe
Maxi Kühn <45176228+maxikuehn@users.noreply.github.com>
2025-01-23 19:44:46 +0100

    initial commit

---
+++ mod_main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+const AUTHORNAME_MODNAME_DIR := "blank-BetterDmgNumbers"
+const AUTHORNAME_MODNAME_LOG_NAME := "blank-BetterDmgNumbers:Main"
+
+var mod_dir_path := ""
+var extensions_dir_path := ""
+
+
+func _init() -> void:
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(AUTHORNAME_MODNAME_DIR)
+	install_script_extensions()
+
+func install_script_extensions() -> void:
+	extensions_dir_path = mod_dir_path.plus_file("extensions")
+
+
+func _ready() -> void:
+	ModLoaderLog.info("Ready!", AUTHORNAME_MODNAME_LOG_NAME)
