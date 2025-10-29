a69bc3da9edf2a879cafc09dc890b800ae7e8ed0
Maximilian Kühn <dev@maxikuehn.de>
2025-10-29 15:56:47 +0100

    Update for New Dawn

--- extensions/visual_effects/floating_text/floating_text_manager.gd
+++ extensions/visual_effects/floating_text/floating_text_manager.gd
@@ -2,9 +2,9 @@ extends "res://visual_effects/floating_text/floating_text_manager.gd"
 
 const MYMOD_LOG = "blank-BetterDmgNumbers"
 
-func _on_unit_took_damage(unit: Unit, value: int, _knockback_direction: Vector2, is_crit: bool, is_dodge: bool, is_protected: bool, armor_did_something: bool, _args: TakeDamageArgs, hit_type: int) -> void :
+func _on_unit_took_damage(unit: Unit, value: int, _knockback_direction: Vector2, is_crit: bool, is_dodge: bool, is_protected: bool, armor_did_something: bool, _args: TakeDamageArgs, hit_type: int, is_one_shot: bool) -> void :
 	if not ProgressData.settings.damage_display:
-		return
+			return
 
 	var color: Color = Color.white
 	var text = format_damage_text(value)
@@ -14,7 +14,7 @@ func _on_unit_took_damage(unit: Unit, value: int, _knockback_direction: Vector2,
 	if unit is Player:
 		always_display = true
 		if value > 0:
-			color = Color.red
+			color = ProgressData.settings.color_negative
 			text = "-" + text
 		elif is_dodge:
 			text = "DODGE"
@@ -24,23 +24,28 @@ func _on_unit_took_damage(unit: Unit, value: int, _knockback_direction: Vector2,
 			need_translate = true
 	elif is_crit:
 		color = Color.yellow
+	elif is_one_shot:
+		always_display = true
+		text = "ONE_SHOT"
+		need_translate = true
 	elif armor_did_something:
 		color = Color.gray
+
 
 	var icon = null if hit_type == HitType.NORMAL else get_special_hit_icon(hit_type)
 
 	display(text, unit.global_position, color, icon, duration, always_display, direction, need_translate)
 
 func format_damage_text(value: int) -> String :
-	var suffix = ["", "K", "M", "B"]
+	var suffix = ["", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "O", "N", "D"]
 	var fvalue = float(value)
 	var p = 0
-	
-	while(fvalue > 1000):
+
+	while(fvalue >= 1000):
 		p += 1
 		fvalue /= 1000
-	
+
 	if(p >= len(suffix)) :
 		return "TOO MUCH DMG"
-	
+
 	return str(stepify(fvalue, 0.01)) + suffix[p]
